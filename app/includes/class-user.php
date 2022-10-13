cfdf9f49072b11a4a5189f2a9811fd25b264b41e
ninoslavjaric <jaricninoslav@gmail.com>
2022-10-13 14:37:59 +0200

    1HI3moYt/185-password-restriction-sign-up-of-user

--- app/includes/class-user.php
+++ app/includes/class-user.php
@@ -16663,9 +16663,9 @@ class User
         } else {
             $args['phone'] = 'null';
         }
-        if (strlen($args['password']) < 6) {
-            throw new Exception(__("Your password must be at least 6 characters long. Please try another"));
-        }
+        // if (strlen($args['password']) < 6) {
+        //     throw new Exception(__("Your password must be at least 6 characters long. Please try another"));
+        // }
         if (!valid_name($args['first_name'])) {
             throw new Exception(__("Your first name contains invalid characters"));
         }
