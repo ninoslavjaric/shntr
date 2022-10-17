1f03face1d525fa67855ae07ae62f5719efb3a08
ninoslavjaric <jaricninoslav@gmail.com>
2022-10-17 14:12:13 +0200

    fixing stripe table

---
+++ mariadb/migrations/1666008697.sql
@@ -0,0 +1,20 @@
+DELIMITER ;;
+create or replace procedure migrator()
+begin
+    set @migration_name = '1666008697';
+
+    if (select count(1) from migrations where migration_name = @migration_name) = 0 then
+        alter table stripe_transactions modify qty int unsigned not null;
+
+        insert into migrations values (@migration_name);
+        select concat('Migration ', @migration_name, ' executed.') as message;
+    else
+        select concat('Migration ', @migration_name, ' already executed.') as message;
+    end if;
+
+end ;;
+delimiter ;
+
+call migrator();
+
+drop procedure if exists migrator;
