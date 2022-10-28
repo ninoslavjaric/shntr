f82a1e7ee3aa7dc55add28098f8f1179e89bd3ee
ninoslavjaric <jaricninoslav@gmail.com>
2022-10-28 08:51:44 +0200

    pGwK5hJc/220-confirmation-mail-should-be-browser-independent

--- app/activation.php
+++ app/activation.php
@@ -16,14 +16,21 @@ if (!isset($_GET['code'])) {
 }
 
 // user access
-if (!$user->_logged_in) {
-	user_login();
-}
+// if (!$user->_logged_in) {
+// 	user_login();
+// }
 
 try {
 
 	// activation
-	$user->activation_email($_GET['code']);
+	if ($user->_logged_id) {
+		$user->activation_email($_GET['code']);
+	}else {
+		global $db, $system;
+		$query = $db->query(sprintf('SELECT user_id from users WHERE user_email_verification_code = %s;', secure($_GET['code']))) or _error("SQL_ERROR_THROWEN");
+		$loginInfo = $query->fetch_row()[0];
+		$db->query(sprintf("UPDATE users SET user_email_verified = '1' WHERE user_id = %s;", secure($loginInfo))) or _error("SQL_ERROR_THROWEN");
+	}
 	redirect();
 } catch (Exception $e) {
 	_error(__("Error"), $e->getMessage());
