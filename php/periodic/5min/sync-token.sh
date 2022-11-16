57ab0ca98c8c588a8c644de81aea3e02bcad08c6
ninoslavjaric <jaricninoslav@gmail.com>
2022-11-16 19:36:25 +0100

    fix file name typo

--- php/periodic/5min/sync-token.sh
+++ php/periodic/5min/sync-token.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 gzip /tmp/*.txt
 php /var/www/html/cli/sync-balances.php "${HOST}" > /tmp/$(date +%s).txt
-php /var/www/html/cli/process_transactions.php "${HOST}" > /tmp/$(date +%s).txt
+php /var/www/html/cli/process_token_transactions.php "${HOST}" > /tmp/$(date +%s).txt
