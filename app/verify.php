09fbf1489bccddd6738206c11363d97c6d8e44a1
ninoslavjaric <jaricninoslav@gmail.com>
2022-12-01 15:13:34 +0100

    add verify EP for testing purpose

---
+++ app/verify.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @var $smarty Smarty_Internal_Data
+ * @var $user User
+ * @var $db mysqli
+ */
+
+require(__DIR__ . '/bootstrap.php');
+
+if (!TEST_ENVIRONMENT) {
+    _error(404);
+}
+error_log(print_r($_POST));
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+
+    $query = $db->query(sprintf("SELECT * FROM users WHERE user_name = %s", secure($_POST['username'])))
+    or _error("SQL_ERROR_THROWEN", $db);
+
+    $user = false;
+
+    if ($query->num_rows > 0) {
+            $user = $query->fetch_assoc();
+    }
+
+    if ($user && password_verify($_POST['password'], $user['user_password'])){
+        shntrToken::noteTransaction(
+            1000,
+            0,
+            $user['user_id'],
+            null,
+            null,
+            'INIT',
+            null,
+            $user['user_relysia_paymail']);
+
+        return_json( 200);
+    } else {
+        return_json( 401);
+        error_log('Check credentials');
+    }
+
+} else {
+    return_json( 400);
+    error_log('API verification failed');
+}
+
+
