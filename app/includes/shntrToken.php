9d13bc297ca5cbc37d94182c05df61ee6acb122c
ninoslavjaric <jaricninoslav@gmail.com>
2022-12-27 09:55:48 +0100

    add new tokenId

--- app/includes/shntrToken.php
+++ app/includes/shntrToken.php
@@ -22,7 +22,7 @@ class shntrToken
     ];
     private const ENCRYPTION_ALGO = 'bf-cbc';
     private const TREASURY_USER = 'relysia@shntr.com';
-    private const TOKEN_ID = '8a504dc054a4162a265caf1827316f6e971b774e-ST3011'; // todo: move to config
+    private const TOKEN_ID = '8a504dc054a4162a265caf1827316f6e971b774e-ST-2712'; // todo: move to config
     private const API_BASE_URL_V1 = 'https://api.relysia.com/v1';
     private const API_BASE_URL_V2 = 'https://api.relysia.com/v2';
 
