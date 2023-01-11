8e49216b62352ca70ab5de34a8d40e657426030b
ninoslavjaric <jaricninoslav@gmail.com>
2023-01-11 20:26:53 +0100

    prepare placeholders for config

--- app/includes/config-example.php
+++ app/includes/config-example.php
@@ -9,23 +9,23 @@
 
 // ** MySQL settings ** //
 /** The name of the database */
-define('DB_NAME', '');
+define('DB_NAME', '{db_name}');
 
 /** MySQL database username */
-define('DB_USER', '');
+define('DB_USER', '{db_user}');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '{db_password]');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '{db_host}');
 
 /** MySQL port */
-define('DB_PORT', '3306');
+define('DB_PORT', '{db_port}');
 
 
 // ** System URL ** //
-define('SYS_URL', ''); // e.g (http://example.com)
+define('SYS_URL', '{sys_url}'); // e.g (http://example.com)
 
 
 // ** i18n settings ** //
@@ -39,15 +39,15 @@ define('DEFAULT_LOCALE', 'en_us');
  * It is strongly recommended that plugin and theme developers use debug
  * in their development environments.
  */
-define('DEBUGGING', false);
+define('DEBUGGING', '{debugging}');
 
 
 /**
  * For developers: log errors in error.log
  */
-define('ERROR_LOGGING', true);
+define('ERROR_LOGGING', '{error_logging}');
 
-define('TEST_ENVIRONMENT', false);
+define('TEST_ENVIRONMENT', '{test_environment}');
 
 
 // ** LICENCE ** //
@@ -57,6 +57,9 @@ define('TEST_ENVIRONMENT', false);
 define('LICENCE_KEY', '');
 
 
-define("REDIS_HOST", 'redis-shntr');
-define('REDIS_HOST_REPLICA', 'redis-shntr');
-define('REDIS_PREFIX', 'redis_prefix:');
+define("WS_ENDPOINT", "{ws_endpoint}");
+define("AWS_REGION", '{aws_region}');
+define("AWS_SQS_QUEUE", '{aws_sqs_queue}');
+define("REDIS_HOST", '{redis_host}');
+define('REDIS_HOST_REPLICA', '{redis_host_replica}');
+define('REDIS_PREFIX', '{redis_prefix}');
